08a58e49f6397dc53bc0ea3b5fa8d7d61edbc14c
honestit <michal.kupisinski@gmail.com>
2020-04-03 15:53:56 +0200

    Testowa odpowiedź z udostępnianiem książki

--- src/main/java/pl/honestit/coderslab/workshops/spring_mvc_rest/controllers/BookController.java
+++ src/main/java/pl/honestit/coderslab/workshops/spring_mvc_rest/controllers/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.honestit.coderslab.workshops.spring_mvc_rest.model.Book;
 
 @RestController
 @RequestMapping("/books")
@@ -12,5 +13,11 @@ public class BookController {
     @GetMapping(value = "/hello", produces = "application/json;charset=UTF-8")
     public String sayHelloInJSON() {
         return "{ \"message\": \"Hello, world\"}";
+    }
+
+    @GetMapping("/hello-book")
+    public Book showBookInJSON() {
+        Book sampleBook = new Book(1L, "898989:909090", "Książka o JSONach", "CodersLab", "CodersLab", "PDF");
+        return sampleBook;
     }
 }
