b265ef5fe94d4a06b9fd6a2108df80923e1cfde6
honestit <michal.kupisinski@gmail.com>
2020-04-04 09:51:45 +0200

    Aktualizacja BookController

--- src/main/java/pl/honestit/coderslab/workshops/spring_mvc_rest/AppConfig.java
+++ src/main/java/pl/honestit/coderslab/workshops/spring_mvc_rest/AppConfig.java
@@ -2,12 +2,23 @@ package pl.honestit.coderslab.workshops.spring_mvc_rest;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.JsonbHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan(basePackageClasses = AppConfig.class)
 @EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
---
+++ src/main/java/pl/honestit/coderslab/workshops/spring_mvc_rest/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package pl.honestit.coderslab.workshops.spring_mvc_rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {}
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
